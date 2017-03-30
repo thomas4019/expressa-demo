7c3956b73b611e7cf3ef3c5cbe13755cfab970b3
Thomas Hansen <th4019@gmail.com>
2017-03-30 01:01:21 -0700

    showing all features

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 var expressa = require('expressa');         // 
 app.use('/api', expressa);                  //
-app.use('/admin', expressa.admin());        // pass expressa.admin({ apiurl:'/myprefix' }) if you're running the api on another host or prefix
+app.use('/admin', expressa.admin({showCriticalFeaturesOnProduction: true}));        // pass expressa.admin({ apiurl:'/myprefix' }) if you're running the api on another host or prefix
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
