75d8f970ffa0f9f186fc908e055e5811ce935d3a
Thomas Hansen <th4019@gmail.com>
2017-03-29 23:54:36 -0700

    added blogpost

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+var expressa = require('expressa');         // 
+app.use('/api', expressa);                  //
+app.use('/admin', expressa.admin());        // pass expressa.admin({ apiurl:'/myprefix' }) if you're running the api on another host or prefix
+
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
